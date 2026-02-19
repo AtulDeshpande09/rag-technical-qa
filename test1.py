58872a92116ef41e43a1504e152989820af91044
Atul Deshpande <deshpandeatul05@gmail.com>
2026-02-19 19:36:04 +0530

    Update test1.py
    fixed data path

--- test1.py
+++ test1.py
@@ -7,7 +7,7 @@ from tqdm import tqdm
 # ======================
 # CONFIG
 # ======================
-TEST_FILE = "data/test.jsonl"
+TEST_FILE = "data/data/test.jsonl"
 MODEL_NAME = "mistralai/Mistral-7B-Instruct-v0.2"
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
